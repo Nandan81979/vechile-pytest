786f60f79bf261ce1c603eec3814b1ae481ef3e1
Nandan81979 <nandanshettar8197@gmail.com>
2025-12-02 12:07:29 +0530

    Update test_vechile.py

--- test_vechile.py
+++ test_vechile.py
@@ -2,11 +2,11 @@ from vehicle import vehicle_info
 
 def test_vehicle_info():
     expected_output = (
-        "Vehicle Number: KA01AB1234\n"
-        "Owner Name: Samarth\n"
+        "Vehicle Number: KA27EJ0147\n"
+        "Owner Name: Nandan\n"
         "Vehicle Type: Car\n"
-        "Year of Manufacture: 2020"
+        "Year of Manufacture: 2021"
     )
 
-    result = vehicle_info("KA01AB1234", "Samarth", "Car", 2020)
+    result = vehicle_info("KA27EJ0147", "Nandan", "Car", 2021)
     assert result == expected_output
